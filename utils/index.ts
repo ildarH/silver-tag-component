942f2300454106fc6705e39aa98c19efc856596b
ildarH <ildar.habibrahmanov@icloud.com>
2022-11-26 22:16:23 +0700

    feat(mock): add mock data

---
+++ utils/index.ts
@@ -0,0 +1,24 @@
+import { ITags } from '../@types';
+
+export const TAG = {
+  OVERVIEW: 'OVERVIEW',
+  HISTORICAL: 'HISTORICAL',
+};
+
+export const ICONS = {
+
+};
+
+export const tagsList: ITags[] = [
+  { tag: TAG.OVERVIEW, icon: 'car' },
+  { tag: TAG.HISTORICAL, icon: 'car' },
+  { tag: 'car', icon: 'car' },
+  { tag: 'car', icon: 'car' },
+  { tag: 'car', icon: 'car' },
+  { tag: 'car', icon: 'car' },
+  { tag: 'car', icon: 'car' },
+  { tag: 'car', icon: 'car' },
+  { tag: 'car', icon: 'car' },
+  { tag: 'car', icon: 'car' },
+  { tag: 'car', icon: 'car' },
+];
