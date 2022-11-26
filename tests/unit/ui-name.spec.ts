cf8ef988ca30f23892cb112775fbcf2d1b0683f7
ildarH <ildar.habibrahmanov@icloud.com>
2022-11-27 03:23:08 +0700

    test(tag): add ui-name test

--- tests/unit/ui-name.spec.ts
+++ tests/unit/ui-name.spec.ts
@@ -3,12 +3,28 @@ import Name from '@/components/Ui/Tag/Name.vue';
 import { TAG } from '@/utils';
 
 const overview = TAG.OVERVIEW;
+const onBus = TAG.ON_BUS;
+const historical = TAG.HISTORICAL;
 
 describe('Name.vue', () => {
-  it('render tag name', () => {
+  it('render tag name Обзорная', () => {
     const name = 'Обзорная';
     const wrapper = shallowMount(Name, {
       propsData: { name: overview },
+    });
+    expect(wrapper.text()).toMatch(name);
+  });
+  it('render tag name На автобусе', () => {
+    const name = 'На автобусе';
+    const wrapper = shallowMount(Name, {
+      propsData: { name: onBus },
+    });
+    expect(wrapper.text()).toMatch(name);
+  });
+  it('render tag name Историческая', () => {
+    const name = 'Историческая';
+    const wrapper = shallowMount(Name, {
+      propsData: { name: historical },
     });
     expect(wrapper.text()).toMatch(name);
   });
