b4f6d6df0054ca765ab49453bba5493c5cb5c2d2
ildarH <ildar.habibrahmanov@icloud.com>
2022-11-27 03:19:03 +0700

    feat(index): add mock index page
    remove unused component

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hello world</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+// @vue/component
+export default Vue.extend({
+  name: 'index-page',
+});
+</script>
+
+<style scoped>
+
+</style>
