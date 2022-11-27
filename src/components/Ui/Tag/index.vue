3993cb09a190369a9c9100565c5b8e07a60e8b2c
ildarH <ildar.habibrahmanov@icloud.com>
2022-11-28 02:15:59 +0700

    feat(tag): update import path, update props
    update import path
    update props
    add data-width to tag
    add styles

--- src/components/Ui/Tag/index.vue
+++ src/components/Ui/Tag/index.vue
@@ -1,30 +1,62 @@
 <template>
-  <div>
-    <ui-icon :icon="ICONS.BUS" />
-    <ui-name :name="TAG.ON_BUS"></ui-name>
-    <ui-icon :icon="ICONS.DIVIDER" />
-  </div>
+  <span class="tag d-inline-block align-center" :id="id" :ref="id" :data-width="width">
+    <ui-icon class="mr-2 icon" v-if="tagItem.icon" :icon="tagItem.icon" />
+    <ui-name :name="tagItem.tag" :value="tagItem.value" />
+    <ui-icon class="icon" :icon="ICONS.DIVIDER" />
+  </span>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, { PropType } from 'vue';
 import UiIcon from '@/components/Ui/Tag/Icon.vue';
 import UiName from '@/components/Ui/Tag/Name.vue';
-import { ICONS, TAG } from '@/utils';
+import {
+  ICONS, TAG, TTag, TTagValue,
+} from '@/@types';
 
 // @vue/component
 export default Vue.extend({
   name: 'base-tag',
   components: { UiName, UiIcon },
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    tagItem: {
+      type: Object as PropType<TTag | TTagValue>,
+      required: true,
+      validator(item: TTag) {
+        return Object.values(TAG).includes(item.tag);
+      },
+    },
+  },
   data() {
     return {
       ICONS,
       TAG,
+      tagEl: null as null | HTMLElement,
+      width: 0,
     };
+  },
+  mounted() {
+    this.tagEl = this.$refs[this.id] as HTMLElement;
+
+    if (this.tagEl) {
+      this.width = Math.ceil(this.tagEl.offsetWidth);
+    }
   },
 });
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.tag {
+  white-space: nowrap;
+  &:hover {
+    cursor: pointer;
+  }
+}
+.icon {
+  width: 24px;
+}
 </style>
