f474b5e632602de333e3007fb109ce31da9c22c2
ildarH <ildar.habibrahmanov@icloud.com>
2022-11-28 02:13:05 +0700

    feat(pages): include mock index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>Hello world</div>
+  <app-main />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import AppMain from '@/components/AppMain.vue';
 
 // @vue/component
 export default Vue.extend({
   name: 'index-page',
+  components: { AppMain },
 });
 </script>
-
-<style scoped>
-
-</style>
